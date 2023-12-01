c0098408f90042ce3989622fdb6a90ad189efb2c
Craig McIlwrath <craigmc08@gmail.com>
2023-12-01 12:30:33 -0500

    add comments for day 1

--- 1/solve.hs
+++ 1/solve.hs
@@ -17,18 +17,23 @@ main :: IO ()
 main = do
   args <- getArgs
 
+  -- Choose the correct algorithm based on which part we want to solve.
   let recoverCalibrationNums = case args of
         ("p1" : _) -> recoverCalibrationNumsFromDigits
         ("p2" : _) -> recoverCalibrationNumsFromDigitsAndWords
         _ -> error "first argument should be `p1' or `p2'"
 
+  -- Read input from stdin and print the sum of the calibration numbers.
   input <- TIO.getContents
   let calibrationNums = recoverCalibrationNums input
   print $ sum calibrationNums
 
+-- | Takes in input and returns a list of the calibration number on each line.
+-- Used in solving part 1 of this puzzle.
 recoverCalibrationNumsFromDigits :: Text -> [Int]
 recoverCalibrationNumsFromDigits text = map recoverCalibrationNum $ T.lines text
   where
+    -- Finds the first and last digit in a string and combines them into a number.
     recoverCalibrationNum :: Text -> Int
     recoverCalibrationNum =
       (read :: String -> Int)
@@ -36,9 +41,13 @@ recoverCalibrationNumsFromDigits text = map recoverCalibrationNum $ T.lines text
         . (T.head &&& T.last)
         . T.filter isDigit
 
+-- | Takes in input and returns a list of the calibraiton number on each line.
+-- Used in solving part 2 of this puzzle.
 recoverCalibrationNumsFromDigitsAndWords :: Text -> [Int]
 recoverCalibrationNumsFromDigitsAndWords = map recoverCalibrationNum . T.lines
   where
+    -- Finds the first and last number in a line (whether that number is written
+    -- as a digit or as a word like `one') and combines them.
     recoverCalibrationNum :: Text -> Int
     recoverCalibrationNum =
       (read :: String -> Int)
@@ -46,10 +55,22 @@ recoverCalibrationNumsFromDigitsAndWords = map recoverCalibrationNum . T.lines
         . (head &&& last)
         . collectDigits
 
+    -- Finds all of the digits in a string. A digit is either a single character
+    -- or a word like `one'. This will find words that share some letters, for
+    -- example:
+    --
+    -- >>> collectDigits "oneightwone"
+    -- [1, 8, 2, 1]
+    --
+    -- which is important for solving the puzzle, even though it isn't super
+    -- clear from the puzzle description!
     collectDigits :: Text -> [Char]
     collectDigits text = case T.uncons text of
       Nothing -> []
       Just (c, cs) ->
+        -- For the uninitiated, there's a fun little optimization based on laziness
+        -- where this "wordAsDigit" variable isn't computed if the first character
+        -- is a digit. Magic.
         let wordAsDigit =
               case find (\(word, _) -> word `T.isPrefixOf` text) digitNames of
                 Nothing -> Nothing
